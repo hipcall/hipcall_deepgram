2568ca9e5a6f4707b50b1009af90028c7e0432ae
Onur Ozgur OZKAN <onur.ozgur.ozkan@bulutfon.com>
2024-01-05 05:26:44 +0000

    Fix docs problem with adding docs() method.

--- mix.exs
+++ mix.exs
@@ -13,7 +13,8 @@ defmodule HipcallDeepgram.MixProject do
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      package: package()
+      package: package(),
+      docs: docs()
     ]
   end
 
